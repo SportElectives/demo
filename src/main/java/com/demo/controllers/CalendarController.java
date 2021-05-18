fe231dfe1e6c53cb88d071104a52c2e16fe5f271
bex-cake <b.turgunov@astanait.edu.kz>
2021-05-19 00:09:12 +0600

    Calendar finished

--- src/main/java/com/demo/controllers/CalendarController.java
+++ src/main/java/com/demo/controllers/CalendarController.java
@@ -1,20 +1,72 @@
 package com.demo.controllers;
 
-import com.demo.repository.ScheduleRepository;
-import org.springframework.http.ResponseEntity;
+import com.demo.models.Elective;
+import com.demo.models.Schedule;
+import com.demo.models.User;
+import com.demo.repository.ElectiveRepository;
+import com.demo.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/timeline")
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.*;
+
+@Controller
+@RequestMapping("/calendar")
 public class CalendarController {
-    private final ScheduleRepository scheduleRepository;
+    private final ElectiveRepository electiveRepository;
+    private final UserRepository userRepository;
 
-    public CalendarController(ScheduleRepository scheduleRepository) {
-        this.scheduleRepository = scheduleRepository;
+    public CalendarController(ElectiveRepository electiveRepository, UserRepository userRepository) {
+        this.electiveRepository = electiveRepository;
+        this.userRepository = userRepository;
     }
 
-    public void getEvents() {
 
+    @GetMapping("/")
+    public String showElective(Model model, Authentication authentication) {
+        User user = userRepository.findByIin(authentication.getName());
+        List<Elective> myElectives = new ArrayList<>();
+        List<Elective> allElectives = electiveRepository.findAll();
+        if (user.getRole().getName().equals("ROLE_PUPIL")) {
+            for (Elective elective : allElectives) {
+                if (elective.hasPupil(user)) {
+                    myElectives.add(elective);
+                }
+            }
+        } else if (user.getRole().getName().equals("ROLE_INSTRUCTOR")) {
+            for (Elective elective : allElectives) {
+                if (elective.getInstructor().equals(user)) {
+                    myElectives.add(elective);
+                }
+            }
+        }
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.MONTH, c.get(Calendar.MONTH) + 1);
+        c.set(Calendar.DAY_OF_MONTH, 0);
+        Date lastDay = c.getTime();
+        c.set(Calendar.MONTH, c.get(Calendar.MONTH));
+        c.set(Calendar.DAY_OF_MONTH, 0);
+        Date prevLastDay = c.getTime();
+        int prevLastDayIndex = prevLastDay.getDay();
+        int lastDayIndex = lastDay.getDay();
+        int nextDays = 7 - lastDayIndex;
+
+        model.addAttribute("lastDay", lastDay.getDate());
+        model.addAttribute("prevLastDay", prevLastDay.getDate());
+        model.addAttribute("prevLastDayIndex", prevLastDayIndex);
+        model.addAttribute("lastDayIndex", lastDay.getDay());
+        model.addAttribute("nextDays", nextDays);
+        model.addAttribute("month", LocalDate.now().getMonth());
+        model.addAttribute("day", LocalDate.now().getDayOfMonth());
+        model.addAttribute("weekDays", DayOfWeek.values());
+        String[] daysOfWeek = new String[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        model.addAttribute("daysOfWeek", daysOfWeek);
+        model.addAttribute("electives", myElectives);
+        return "calendar";
     }
 }
--- src/main/java/com/demo/controllers/MainController.java
+++ src/main/java/com/demo/controllers/MainController.java
@@ -12,9 +12,4 @@ public class MainController {
     public String index() {
         return "index";
     }
-
-    @GetMapping("calendar")
-    public String calendar() {
-        return "calendar";
-    }
 }
